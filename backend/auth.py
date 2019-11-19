9df030d6342b2b32f33482fa16320a32b62ced31
oddjob79 <rmeturner@gmail.com>
2019-11-19 14:49:04 +0100

    added auth0 config to auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'coffee-shop-rt.eu.auth0.com'
+AUTH0_DOMAIN = 'skill-exchange.eu.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee-shop'
+API_AUDIENCE = 'skillsexchange'
 
 ## AuthError Exception
 '''
@@ -21,15 +21,6 @@ class AuthError(Exception):
 
 
 ## Auth Header
-
-# '''
-# @TODO implement get_token_auth_header() method
-#     it should attempt to get the header from the request
-#         it should raise an AuthError if no header is present
-#     it should attempt to split bearer and the token
-#         it should raise an AuthError if the header is malformed
-#     return the token part of the header
-# '''
 
 # Method adapted from udacity coursework - provided by Gabe Ruttner
 def get_token_auth_header():
@@ -66,18 +57,6 @@ def get_token_auth_header():
     return(header_parts[1])
 
 
-# '''
-# @TODO implement check_permissions(permission, payload) method
-#     @INPUTS
-#         permission: string permission (i.e. 'post:drink')
-#         payload: decoded jwt payload
-#
-#     it should raise an AuthError if permissions are not included in the payload
-#         !!NOTE check your RBAC settings in Auth0
-#     it should raise an AuthError if the requested permission string is not in the payload permissions array
-#     return true otherwise
-# '''
-
 # Method taken from the udacity coursework - provided by Gabe Ruttner
 def check_permissions(permission, payload):
     # raise an AuthError if permissions are not included in the payload
@@ -93,20 +72,6 @@ def check_permissions(permission, payload):
             'description': 'Permission not in payload.'
         }, 401)
     return True
-
-# '''
-# @TODO implement verify_decode_jwt(token) method
-#     @INPUTS
-#         token: a json web token (string)
-#
-#     it should be an Auth0 token with key id (kid)
-#     it should verify the token using Auth0 /.well-known/jwks.json
-#     it should decode the payload from the token
-#     it should validate the claims
-#     return the decoded payload
-#
-#     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
-# '''
 
 # Method taken from the udacity coursework - provided by Gabe Ruttner
 def verify_decode_jwt(token):
@@ -174,17 +139,6 @@ def verify_decode_jwt(token):
                 'description': 'Unable to find the appropriate key.'
             }, 400)
 
-# '''
-# @TODO implement @requires_auth(permission) decorator method
-#     @INPUTS
-#         permission: string permission (i.e. 'post:drink')
-#
-#     it should use the get_token_auth_header method to get the token
-#     it should use the verify_decode_jwt method to decode the jwt
-#     it should use the check_permissions method validate claims and check the requested permission
-#     return the decorator which passes the decoded payload to the decorated method
-# '''
-
 #  Replaced with method given during coursework - provided by Gabe Ruttner
 def requires_auth(permission=''):
     # decorators take in the function (f) which they decorate
@@ -209,16 +163,3 @@ def requires_auth(permission=''):
         # requires_auth returns the wrapper
         return wrapper
     return requires_auth_decorator
-
-
-# def requires_auth(permission=''):
-#     def requires_auth_decorator(f):
-#         @wraps(f)
-#         def wrapper(*args, **kwargs):
-#             token = get_token_auth_header()
-#             payload = verify_decode_jwt(token)
-#             check_permissions(permission, payload)
-#             return f(payload, *args, **kwargs)
-#
-#         return wrapper
-#     return requires_auth_decorator
